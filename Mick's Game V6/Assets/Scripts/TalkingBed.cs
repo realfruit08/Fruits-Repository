55e336f04b09f31d5fdb50e9e8ea425c9261fde7
realfruit08 <mick.mcnulty08@gmail.com>
2024-11-24 10:42:39 -0500

    Mick's Game V6
    Narrative Lab7 - Quest Design Complete

---
+++ Mick's Game V6/Assets/Scripts/TalkingBed.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class TalkingBed : MonoBehaviour
+{
+    bool cantalk = false;
+    public DialogueRunner runner;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E) && cantalk)
+        {
+            runner.Stop();
+            runner.StartDialogue("BedDialogue");
+
+
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D othercollider)
+    {
+        if (othercollider.gameObject.name == ("Kelsey"))
+        {
+            cantalk = true;
+
+        }
+        else
+        {
+            cantalk = false;
+
+        }
+
+
+
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        cantalk = false;
+    }
+
+
+}
